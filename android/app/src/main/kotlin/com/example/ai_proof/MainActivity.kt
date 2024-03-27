0c2dfac86ef44a0191cd12b0d53a960068527a0c
Johcel Gene T. Bitara <jbitara@gbox.adnu.edu.ph>
2024-03-27 14:38:31 +0800

    Renamed project name

--- android/app/src/main/kotlin/com/example/ai_proof/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_proof/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ai_proof
+package com.example.aiproof
 
 import io.flutter.embedding.android.FlutterActivity
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import path_provider_foundation
 import pdf_render
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
