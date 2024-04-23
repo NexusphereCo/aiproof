981e4c8101796f8b8b630fd0248e5120f671510e
MD-Garcera <md.garcera16@gmail.com>
2024-04-23 23:54:32 +0800

    Sample Document

---
+++ lib/database/mock_database.dart
@@ -0,0 +1,8 @@
+import 'package:aiproof/data/models/document_model.dart';
+
+List<DocumentModel> mockDatabase = [
+  DocumentModel(fileName: 'IMG_2024_01221.png', fileSize: '260KB', dateUploaded: '01-01-2024', image: 'assets/imgs/doc/sample_doc1.png'),
+  DocumentModel(fileName: 'IMG_2024_01222.png', fileSize: '349KB', dateUploaded: '01-02-2024', image: 'assets/imgs/doc/sample_doc2.png'),
+  DocumentModel(fileName: 'IMG_2024_01223.png', fileSize: '401KB', dateUploaded: '01-03-2024', image: 'assets/imgs/doc/sample_doc3.png'),
+  DocumentModel(fileName: 'IMG_2024_01224.png', fileSize: '286KB', dateUploaded: '01-04-2024', image: 'assets/imgs/doc/sample_doc4.png'),
+];
