83c782de5bfcd514e2c76a162d5e0b808649bdef
MD-Garcera <md.garcera16@gmail.com>
2024-05-08 22:41:03 +0800

    Update HomeScreen and InputScreen

--- lib/modules/home/index.dart
+++ lib/modules/home/index.dart
@@ -1,6 +1,6 @@
+import 'package:aiproof/business_logic/document/document_bloc.dart';
 import 'package:aiproof/constants/assets.dart';
 import 'package:aiproof/constants/sizes.dart';
-import 'package:aiproof/database/mock_database.dart';
 import 'package:aiproof/modules/home/components/header_logo.dart';
 import 'package:aiproof/modules/home/components/search_bar.dart';
 import 'package:aiproof/modules/home/components/view_toggle.dart';
@@ -8,8 +8,9 @@ import 'package:aiproof/modules/home/enums/doc_view.dart';
 import 'package:aiproof/modules/home/widgets/carousel/carousel_view.dart';
 import 'package:aiproof/modules/home/widgets/grid/grid_view.dart';
 import 'package:aiproof/modules/home/widgets/list/list_view.dart';
-import 'package:aiproof/widgets/layouts/appbar_bottom.dart';
+import 'package:aiproof/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:remixicon/remixicon.dart';
 
@@ -21,7 +22,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _searchController = TextEditingController();
   DocView _currentView = DocView.carousel;
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<DocumentBloc>().add(LoadDocumentEvent());
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   void _handleViewChange(DocView newView) {
     setState(() {
@@ -32,107 +46,80 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            fit: StackFit.passthrough,
+      body: BlocBuilder<DocumentBloc, DocumentState>(
+        builder: (context, state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Positioned(
-                child: SvgPicture.asset(Asset.heading, fit: BoxFit.fill),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(Global.paddingBody),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const HeaderLogo(),
-                    const SizedBox(height: Spacing.xl),
-                    const APSearchBar(),
-                    const SizedBox(height: Spacing.xxxl),
-                    ViewToggle(
-                      documentView: _currentView,
-                      onViewChanged: _handleViewChange,
+              Stack(
+                fit: StackFit.passthrough,
+                children: [
+                  Positioned(
+                    child: SvgPicture.asset(Asset.heading, fit: BoxFit.fill),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(Global.paddingBody),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const HeaderLogo(),
+                        const SizedBox(height: Spacing.xl),
+                        const APSearchBar(),
+                        const SizedBox(height: Spacing.xxxl),
+                        ViewToggle(
+                          documentView: _currentView,
+                          onViewChanged: _handleViewChange,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: () {
+                  if (state is LoadedDocumentState) {
+                    if (_currentView == DocView.carousel) {
+                      return APCarouselView(documents: state.documents);
+                    } else if (_currentView == DocView.list) {
+                      return APListView(documents: state.documents);
+                    } else if (_currentView == DocView.grid) {
+                      return APGridView(documents: state.documents);
+                    }
+                    return Container();
+                  } else if (state is FilteredDocument) {
+                    if (_currentView == DocView.carousel) {
+                      return APCarouselView(documents: state.documents);
+                    } else if (_currentView == DocView.list) {
+                      return APListView(documents: state.documents);
+                    } else if (_currentView == DocView.grid) {
+                      return APGridView(documents: state.documents);
+                    }
+                    return Container();
+                  } else if (state is ErrorDocument) {
+                    return Center(
+                      child: Text(state.message),
+                    );
+                  } else if (state is InitialDocumentState) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }(),
               ),
             ],
-          ),
-          Expanded(
-            child: () {
-              if (_currentView == DocView.carousel) {
-                return APCarouselView(documents: mockDatabase);
-              } else if (_currentView == DocView.list) {
-                return APListView(documents: mockDatabase);
-              } else if (_currentView == DocView.grid) {
-                return APGridView(documents: mockDatabase);
-              }
-              // Add a default view in case none of the conditions match
-              return Container();
-            }(),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // open a modalsheet
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            builder: (BuildContext context) {
-              return SizedBox(
-                height: 220,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      // add a line
-                      Container(
-                        margin: const EdgeInsets.symmetric(vertical: Spacing.md),
-                        height: 4,
-                        width: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      ListTile(
-                        leading: const Icon(Remix.speak_line, size: APSize.xl),
-                        title: const Text('Text-to-Speech'),
-                        onTap: () {
-                          // Handle text-to-speech option
-                        },
-                      ),
-                      ListTile(
-                        leading: const Icon(Remix.gallery_line, size: APSize.xl),
-                        title: const Text('Scan from Image'),
-                        onTap: () {
-                          // Handle AI checker option
-                        },
-                      ),
-                      ListTile(
-                        leading: const Icon(Remix.brain_line, size: APSize.xl),
-                        title: const Text('AI Checker'),
-                        onTap: () {
-                          // Handle AI checker option
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
           );
         },
-        child: const Icon(
-          Remix.camera_line,
-          size: APSize.xl,
-        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: const APAppBarBottom(activeIndex: 1),
-      resizeToAvoidBottomInset: false,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Remix.add_line),
+        onPressed: () {
+          Navigator.of(context).push(
+            createRoute(route: Routes.input),
+          );
+        },
+      ),
     );
   }
 }
--- lib/modules/input/index.dart
+++ lib/modules/input/index.dart
@@ -1,125 +1,143 @@
-import 'package:aiproof/constants/assets.dart';
+import 'package:aiproof/business_logic/document/document_bloc.dart';
 import 'package:aiproof/constants/colors.dart';
 import 'package:aiproof/constants/sizes.dart';
 import 'package:aiproof/constants/typography.dart';
-import 'package:aiproof/modules/home/components/header_logo.dart';
-import 'package:aiproof/modules/home/components/search_bar.dart';
+import 'package:aiproof/data/models/document_model.dart';
 import 'package:aiproof/widgets/common/buttons.dart';
-import 'package:aiproof/widgets/layouts/appbar_bottom.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:remixicon/remixicon.dart';
+import 'package:intl/intl.dart';
 
-class InputScreen extends StatelessWidget {
-  const InputScreen({super.key});
+class InputScreen extends StatefulWidget {
+  final DocumentModel? document;
+
+  const InputScreen({super.key, this.document});
+
+  @override
+  _InputScreenState createState() => _InputScreenState();
+}
+
+class _InputScreenState extends State<InputScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _titleController;
+  late TextEditingController _contentController;
+  late FocusNode _titleFocusNode;
+  late FocusNode _contentFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.document?.title ?? '');
+    _contentController = TextEditingController(text: widget.document?.content ?? '');
+    _titleFocusNode = FocusNode();
+    _contentFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    _titleFocusNode.dispose();
+    _contentFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            fit: StackFit.passthrough,
-            children: [
-              Positioned(
-                child: SvgPicture.asset(Asset.heading, fit: BoxFit.fill),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<DocumentBloc>().add(DeleteDocumentEvent(widget.document?.id ?? 0));
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Remix.delete_bin_line,
+            ),
+          ),
+          if (_titleFocusNode.hasFocus || _contentFocusNode.hasFocus)
+            InkWell(
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  final document = DocumentModel(
+                    id: widget.document?.id,
+                    title: _titleController.text,
+                    content: _contentController.text,
+                    createdAt: DateTime.now(),
+                  );
+
+                  if (widget.document == null) {
+                    context.read<DocumentBloc>().add(CreateDocumentEvent(document));
+                  } else {
+                    context.read<DocumentBloc>().add(UpdateDocumentEvent(document));
+                  }
+                  Navigator.pop(context);
+                }
+              },
+              child: APTypography.h3(
+                'Done',
+                color: APColor.dark,
+                fontWeight: APFontWeight.regular,
               ),
-              const Padding(
-                padding: EdgeInsets.all(Global.paddingBody),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    HeaderLogo(),
-                    SizedBox(height: Spacing.xl),
-                    APSearchBar(),
-                  ],
+            ),
+          const SizedBox(width: APSize.lg),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Global.paddingBody),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              APTypography.base(widget.document?.createdAt.toIso8601String() ?? '', color: APColor.dark.withOpacity(0.5)),
+              TextField(
+                controller: _titleController,
+                focusNode: _titleFocusNode,
+                style: const TextStyle(
+                  fontWeight: APFontWeight.bold,
+                  fontSize: APFontSize.h1,
+                  fontFamily: APTypography.fontFamily,
                 ),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  hintText: 'Title',
+                  counterText: '',
+                ),
+
+                minLines: 1,
+                maxLines: 3,
+                maxLengthEnforcement: MaxLengthEnforcement.truncateAfterCompositionEnds,
+                maxLength: 255, // Add this line to hide the character count
+              ),
+              TextField(
+                controller: _contentController,
+                focusNode: _contentFocusNode,
+                style: TextStyle(
+                  fontWeight: APFontWeight.regular,
+                  fontSize: APFontSize.normal,
+                  fontFamily: APTypography.fontFamily,
+                  color: APColor.dark,
+                ),
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  hintText: 'Write something here...',
+                  counterText: '',
+                ),
+                maxLength: 8000,
+                maxLengthEnforcement: MaxLengthEnforcement.truncateAfterCompositionEnds,
+                maxLines: null,
               ),
             ],
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(Global.paddingBody),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Remix.arrow_left_wide_line),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      const Text('Back'),
-                    ],
-                  ),
-                  APTypography.h1("Scan Input"),
-                  const SizedBox(height: Spacing.md),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: "Content",
-                        hintText: "Enter the content to be scanned",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(APBorderRadius.md),
-                          borderSide: BorderSide(
-                            color: APColor.dark.withOpacity(0.5),
-                            width: 1.0,
-                          ),
-                        ),
-                        hintStyle: TextStyle(color: APColor.dark.withOpacity(0.7), fontWeight: APFontWeight.thin),
-                        labelStyle: TextStyle(color: APColor.dark),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(APBorderRadius.md),
-                          borderSide: BorderSide(
-                            color: APColor.dark[300]!,
-                            width: 1.0,
-                          ),
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.always),
-                    minLines: 10,
-                    maxLines: 15,
-                    readOnly: false,
-                  ),
-                  const SizedBox(height: Spacing.xxl),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(80.0),
-                        ),
-                      ),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.all(0.0)),
-                    ),
-                    child: Ink(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(colors: [Color.fromRGBO(25, 151, 156, 1), Color.fromRGBO(22, 114, 144, 1)]),
-                        borderRadius: BorderRadius.all(Radius.circular(80.0)),
-                      ),
-                      child: Container(
-                        constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0), // min sizes for Material buttons
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(Spacing.lg),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            APTypography.base('Verify', color: APColor.light),
-                            const SizedBox(width: Spacing.md),
-                            Icon(Remix.verified_badge_line, color: APColor.light, size: APSize.xl),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
-      bottomNavigationBar: const APAppBarBottom(activeIndex: 2),
     );
   }
 }
