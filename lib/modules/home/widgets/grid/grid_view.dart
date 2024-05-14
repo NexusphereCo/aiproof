74b9a4e6070ae7f821df55046e3f7bfb9c226768
Johcel Gene T. Bitara <jbitara@gbox.adnu.edu.ph>
2024-05-14 19:17:50 +0800

    Integrated view cards within parent widget

--- lib/modules/home/widgets/grid/grid_view.dart
+++ lib/modules/home/widgets/grid/grid_view.dart
@@ -1,12 +1,63 @@
+import 'package:aiproof/constants/colors.dart';
 import 'package:aiproof/constants/sizes.dart';
+import 'package:aiproof/constants/typography.dart';
 import 'package:aiproof/data/models/document_model.dart';
-import 'package:aiproof/modules/home/widgets/grid/components/grid_view_card.dart';
+import 'package:aiproof/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class APGridView extends StatelessWidget {
   final List<DocumentModel> documents;
 
   const APGridView({super.key, required this.documents});
+
+  Widget _card(DocumentModel document, BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          createRoute(route: Routes.input, args: document),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(APBorderRadius.md),
+          border: Border.all(color: APColor.primary.withOpacity(0.1)),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(APBorderRadius.md),
+          child: Stack(
+            children: <Widget>[
+              Image.memory(
+                document.thumbnail!,
+                fit: BoxFit.cover,
+                width: 1000.0,
+                alignment: Alignment.topCenter,
+              ),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  height: 55,
+                  color: APColor.light,
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: APTypography.h4(document.title, cutOverflow: true, maxLines: 1, fontWeight: APFontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +70,7 @@ class APGridView extends StatelessWidget {
       ),
       itemCount: documents.length,
       itemBuilder: (context, index) {
-        return GridViewCard(document: documents[index]);
+        return _card(documents[index], context);
       },
     );
   }
--- lib/modules/home/widgets/list/list_view.dart
+++ lib/modules/home/widgets/list/list_view.dart
@@ -1,12 +1,62 @@
+import 'package:aiproof/bloc/document/document_bloc.dart';
+import 'package:aiproof/constants/colors.dart';
 import 'package:aiproof/constants/sizes.dart';
+import 'package:aiproof/constants/typography.dart';
 import 'package:aiproof/data/models/document_model.dart';
-import 'package:aiproof/modules/home/widgets/list/components/list_view_card.dart';
+import 'package:aiproof/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 class APListView extends StatelessWidget {
   final List<DocumentModel> documents;
 
   const APListView({super.key, required this.documents});
+
+  Widget _card(DocumentModel document, BuildContext context) {
+    return BlocBuilder<DocumentBloc, DocumentState>(builder: (context, state) {
+      return InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            createRoute(route: Routes.input, args: document),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(APBorderRadius.md),
+            border: Border.all(color: APColor.primary.withOpacity(0.1)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.memory(
+                document.thumbnail!,
+                width: 100,
+                height: 80,
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+              ),
+              const SizedBox(width: Spacing.sm),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: APTypography.h4(document.title, cutOverflow: true, maxLines: 1, fontWeight: APFontWeight.bold),
+                  ),
+                  const SizedBox(height: Spacing.xs), // Add this line
+                  // APTypography.base(document.fileSize, color: APColor.dark.withOpacity(0.5)),
+                  APTypography.small(DateFormat('MMMM d, yyyy - h:mm a').format(document.createdAt), color: APColor.dark.withOpacity(0.5)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +66,7 @@ class APListView extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsetsDirectional.only(bottom: Spacing.md),
-          child: ListViewCard(
-            document: documents[index],
-          ),
+          child: _card(documents[index], context),
         );
       },
     );
